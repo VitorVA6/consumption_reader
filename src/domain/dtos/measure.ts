f8987322f595facfc792e7a78e6786f7bf8c8259
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 09:48:54 -0300

    feat: cria o dto para atualização de measure

--- src/domain/dtos/measure.ts
+++ src/domain/dtos/measure.ts
@@ -4,3 +4,9 @@ export interface CreateMeasureDto {
   measure_datetime: string,
   measure_type: 'WATER' | 'GAS'
 }
+
+export interface UpdateMeasureDto {
+  measure_uuid: string,
+  confirmed_value: number,
+}
+

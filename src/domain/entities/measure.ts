7ed7ca013d169625ee11f3c8d916f88486750ede
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 09:54:47 -0300

    refactor: ajusta os campos da entidade measure para o padrão snake_case

--- src/domain/entities/measure.ts
+++ src/domain/entities/measure.ts
@@ -1,9 +1,9 @@
 export default interface Measure {
-  id: string,
-  value: number,
-  type: 'GAS' | 'WATER',
-  datetime: Date,
-  hasConfirmed: boolean,
-  imageUrl: string,
-  clientCode: string,
+  measure_uuid: string,
+  measure_value: number,
+  measure_type: 'GAS' | 'WATER',
+  measure_datetime: Date,
+  has_confirmed: boolean,
+  image_url: string,
+  customer_code: string,
 }

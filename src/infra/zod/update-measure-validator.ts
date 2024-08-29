968487137b5094529c3e2b8665557f34581637f2
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 16:39:27 -0300

    feat: implementa o validator do dto de atualizar leitura

---
+++ src/infra/zod/update-measure-validator.ts
@@ -0,0 +1,18 @@
+import { z, ZodError } from 'zod';
+import { UpdateMeasureReqDto } from '../../domain/dtos/measure';
+import InvalidDataError from '../../application/errors/invalid-data-error';
+
+export default function update_measure_validator(data: unknown): UpdateMeasureReqDto {
+  const zod_schema = z.object({
+    measure_uuid: z.string(),
+    confirmed_value: z.number().int(),
+  });
+
+  try {
+    const result = zod_schema.parse(data);
+    return result;
+  } catch (err) {
+    if (err instanceof ZodError) throw new InvalidDataError(err.errors[0].message);
+    throw Error();
+  }
+}

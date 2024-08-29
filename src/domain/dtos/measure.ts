0593fb0b3db5ccf874c71a983128850bcec1b908
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 09:49:06 -0300

    feat: cria o dto para listagem de measure

--- src/domain/dtos/measure.ts
+++ src/domain/dtos/measure.ts
@@ -10,3 +10,15 @@ export interface UpdateMeasureDto {
   confirmed_value: number,
 }
 
+export interface ListMeasureDto {
+  custome_code: string,
+  measures: [
+    {
+      measure_uuid: string,
+      measure_datetime: Date,
+      measure_type: string,
+      has_confirmed:boolean,
+      image_url: string
+    },
+  ]
+}

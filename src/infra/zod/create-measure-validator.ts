ddbf114b3dd2c82cc7ad40f9cd67340430d0b0bf
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 16:39:47 -0300

    feat: implementa o validador do dto de criar leitura

---
+++ src/infra/zod/create-measure-validator.ts
@@ -0,0 +1,20 @@
+import { z, ZodError } from 'zod';
+import { CreateMeasureReqDto } from '../../domain/dtos/measure';
+import InvalidDataError from '../../application/errors/invalid-data-error';
+
+export default function create_measure_validator(data: unknown): CreateMeasureReqDto {
+  const zod_schema = z.object({
+    image: z.string().base64(),
+    customer_code: z.string(),
+    measure_datetime: z.coerce.date(),
+    measure_type: z.enum(['GAS', 'WATER']),
+  });
+
+  try {
+    const result = zod_schema.parse(data);
+    return result;
+  } catch (err) {
+    if (err instanceof ZodError) throw new InvalidDataError(err.errors[0].message);
+    throw Error();
+  }
+}

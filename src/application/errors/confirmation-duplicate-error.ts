7739bbb419b03f43067d5ebfb4408a9168b16819
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 11:46:36 -0300

    feat: cria classe para error de confirmação duplicada

---
+++ src/application/errors/confirmation-duplicate-error.ts
@@ -0,0 +1,7 @@
+import ApplicationError from './application-error';
+
+export default class ConfirmationDuplicateError extends ApplicationError {
+  constructor() {
+    super(409, 'CONFIRMATION_DUPLICATE', 'Leitura do mês já realizada');
+  }
+}

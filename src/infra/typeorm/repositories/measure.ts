2016555c8c1a3c9d11e35fe871941ef424a2deb9
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 17:51:33 -0300

    feat: implementa o repositório de measure

---
+++ src/infra/typeorm/repositories/measure.ts
@@ -0,0 +1,56 @@
+import { Raw, Repository } from 'typeorm';
+import TypeormMeasure from '../entities/measure';
+import AppDataSource from '../data-source';
+import Measure from '../../../domain/entities/measure';
+import IMeasureRepository from '../../../application/repositories/measure';
+
+export default class PostgreMeasureRepository implements IMeasureRepository {
+  private measure_table: Repository<TypeormMeasure> = AppDataSource
+    .getRepository(TypeormMeasure);
+
+  async create(measure_data: Omit<Measure, 'measure_uuid'>): Promise<Measure> {
+    const newMeasure = this.measure_table.create(measure_data);
+    const savedMeasure = await this.measure_table.save(newMeasure);
+    return savedMeasure;
+  }
+
+  async update(measure_data: Measure): Promise<void> {
+    await this.measure_table.update(measure_data.measure_uuid, measure_data);
+  }
+
+  async find_by_id(measure_uuid: string): Promise<Measure | null> {
+    const found_measure = await this.measure_table.findOne({
+      where: {
+        measure_uuid,
+      },
+    });
+    return found_measure;
+  }
+
+  async find_by_customer(
+    customer_code: string,
+    measure_type?: 'GAS' | 'WATER',
+  ): Promise<Omit<Measure, 'customer_code' | 'measure_value'>[]> {
+    const found_measures = await this.measure_table.find({
+      where: measure_type ? {
+        customer_code,
+        measure_type,
+      } : {
+        customer_code,
+      },
+    });
+
+    return found_measures;
+  }
+
+  async find_by_date_and_type(date: Date, measure_type: 'GAS' | 'WATER'): Promise<Measure | null> {
+    const record = await this.measure_table.findOne({
+      where: {
+        measure_type,
+        measure_datetime: Raw((alias) => `DATE_TRUNC('month', ${alias}) = DATE_TRUNC('month', '${date.toISOString()}')`),
+      },
+    });
+
+    return record;
+  }
+}

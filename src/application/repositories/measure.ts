8063297cd495d60f4bc003cbd2dde89bed0d98df
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 21:41:29 -0300

    fix: corrige método de filtrar por date, type e customer

--- src/application/repositories/measure.ts
+++ src/application/repositories/measure.ts
@@ -5,5 +5,5 @@ export default interface IMeasureRepository {
   update: (data: Measure) => Promise<void>
   find_by_id: (measure_uuid: string) => Promise<Measure | null>
   find_by_customer: (customer_code: string, measure_type?: 'GAS' | 'WATER') => Promise<Omit<Measure, 'customer_code' | 'measure_value'>[]>
-  find_by_date_and_type: (date: Date, type: 'GAS' | 'WATER') => Promise<Measure | null>
+  find_by_date_and_type_and_customer: (date: Date, type: 'GAS' | 'WATER', customer_code: string) => Promise<Measure | null>
 }

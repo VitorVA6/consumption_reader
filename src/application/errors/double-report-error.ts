8b5ab9b41ec009f5d71e5a258551bfeb9832e082
vitorva6 <vitorandrade@ecomp.uefs.br>
2024-08-29 21:40:50 -0300

    fix: corrige descrição do erro double report

--- src/application/errors/double-report-error.ts
+++ src/application/errors/double-report-error.ts
@@ -2,6 +2,6 @@ import ApplicationError from './application-error';
 
 export default class DoubleReportError extends ApplicationError {
   constructor() {
-    super(409, 'INVALID_DATA', 'Leitura do mês já realizada');
+    super(409, 'DOUBLE_REPORT', 'Leitura do mês já realizada');
   }
 }
